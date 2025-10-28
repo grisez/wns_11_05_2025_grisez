9f44c28d420c02b60f23b492a948d837ac459ebb
cindy grisez <grisezcindy@gmail.com>
2025-10-28 11:04:36 +0100

    Setud apollo Client

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,15 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+// src/main.tsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ApolloProvider } from "@apollo/client";
+import client from "./apolloClient";
+import "./index.css"; // si tu as un CSS global
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
+    </React.StrictMode>
+);
